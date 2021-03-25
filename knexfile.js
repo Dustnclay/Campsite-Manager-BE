4cda53ed9ab03970f83f6bbe586098346be94458
Dustin <DustinTiner@Emilys-MacBook-Air.local>
2021-03-24 21:26:52 -0500

    heroku db up

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
+      database: 'postgres://putbscmtbqlsxg:9a0370c3206dc193ca28dea86d0ed837cddba667435e83fafa995b566d6e5f28@ec2-23-21-229-200.compute-1.amazonaws.com:5432/d70vnfvun121o5',
       // user:     'username',
       // password: ''
     },
