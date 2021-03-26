68689d4f2251e4bbb8d17c5d59556e3b42fb56e0
Dustin <DustinTiner@Emilys-MacBook-Air.local>
2021-03-25 20:15:36 -0500

    quotes removed from process.env

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   staging: {
     client: 'pg',
-    connection: 'process.env.DATABASE_URL',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations' 
     },
