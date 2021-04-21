42cfd4e44d93947fdb3c0b978e84803d8bed1de6
Dustin <DustinTiner@Emilys-MacBook-Air.local>
2021-04-20 21:02:52 -0500

    Data to _data

--- data/migrations/20210316224833_collectData.js
+++ data/migrations/20210316224833_collectData.js
@@ -1,7 +1,7 @@
 
 
     exports.up = function(knex, Promise) {
-        return knex.schema.createTable("formsData", table => {
+        return knex.schema.createTable("forms_data", table => {
             table.increments();
             table.string('location').notNullable();
             table.string('collector').notNullable();
