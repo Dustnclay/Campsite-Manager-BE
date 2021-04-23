c46b0a52373026a330e726d1dc08be7dff0db229
Dustin Tiner <60906954+Dustnclay@users.noreply.github.com>
2021-04-22 20:07:44 -0500

    Update FormRouter.js
    all elements added to form

--- server/FormRouter.js
+++ server/FormRouter.js
@@ -15,7 +15,40 @@ router.post('/', (req,res) => {
 //     const newForm = JSON.stringify(req.body)
 //     console.log("entering post in form model",newForm)
     const newForm = req.body
-    FormModel.postNew({location:newForm.location})
+    FormModel.postNew(
+        {
+            location:newForm.location,
+            collector:newForm.collector,
+            survey:newForm.survey,
+            ground_disturbance:newForm.groundDisturbance,
+            disturbed_area:newForm.disturbedArea,
+            trees_damaged:newForm.treesDamaged,
+            near_water:newForm.nearWater,
+            near_trail:newForm.nearTrail,        
+            erosion:newForm.erosion,
+            fire_rings:newForm.fireRings,
+            improvements:newForm.improvements,        
+            contained:newForm.contained,
+            residential:newForm.residential,
+            trash:newForm.trash,
+            human_waste:newForm.humanWaste,
+            social_trail:newForm.socialTrail,
+            social_road:newForm.socialRoad,
+            site_description:newForm.siteDescription,
+            need_trash_pickup:newForm.needTrashPickup,
+            need_signage_fix:newForm.needSignageFix,
+            need_containment_repair:newForm.needContainmentRepair,
+            need_fire_ring_removal:newForm.needFireRingRemoval,
+            done_trash_pickup:newForm.doneTrashPickup,
+            done_signage_fix:newForm.doneSignageFix,
+            done_containment_repair:newForm.doneContainmentRepair,
+            done_fire_ring_removal:newForm.doneFireRingRemoval,
+            date:newForm.date,
+            hours:newForm.hours,
+            notes:newForm.notes,
+            image:newForm.image
+        }
+    )
     .then(form => {
         res.status(200).json("form post successful")
     })
